56e1a4aae92439333fe05593c084e700f89460da
TeBeCo <tebeco@gmail.com>
2023-09-22 11:52:28 +0400

    add the AppUrlListener into the BrowserRouter

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { IonApp, setupIonicReact } from '@ionic/react';
 import './App.css'
+import { AppUrlListener } from './AppUrlListener';
 
 setupIonicReact();
 
@@ -8,6 +9,7 @@ const App = () => {
   return (
     <IonApp>
       <BrowserRouter>
+        <AppUrlListener></AppUrlListener>
         <Switch>
           <Route exact path="/" render={() => (<>this is the root page</>)} />
         </Switch>
