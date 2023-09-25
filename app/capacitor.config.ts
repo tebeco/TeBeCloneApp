6680c3c53075ea5f4018cb30452bbad8a0d80064
TeBeCo <tebeco@gmail.com>
2023-09-25 23:42:58 +0400

    make capacitor.config.ts re-use .env hostname/scheme

--- app/capacitor.config.ts
+++ app/capacitor.config.ts
@@ -1,11 +1,15 @@
 import { CapacitorConfig } from '@capacitor/cli';
+import dotenv from "dotenv";
+
+dotenv.configDotenv();
 
 const config: CapacitorConfig = {
   appId: 'com.tebeclone.app',
   appName: 'tebeclone-app',
   webDir: 'dist',
   server: {
-    androidScheme: 'https'
+    androidScheme: process.env.VITE_MOBILE_SCHEME,
+    hostname: process.env.VITE_MOBILE_HOSTNAME
   }
 };
 
