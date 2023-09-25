e8ade16b4ce481d20f0ff0c4acf1abf70450623b
TeBeCo <tebeco@gmail.com>
2023-09-26 00:10:07 +0400

    OidcCallback page now SignIn the user in the SessionContext

--- app/src/OidcCallback.tsx
+++ app/src/OidcCallback.tsx
@@ -1,3 +1,15 @@
+import { useEffect } from "react"
+import { useHistory } from "react-router";
+import { useSessionContext } from './contexts/SessionContext';
+
 export const OidcCallback = () => {
+  const [, setSessionContext] = useSessionContext()
+  const history = useHistory();
+
+  useEffect(() => {
+    setSessionContext({ userName: "FooBar" });
+    history.push('/');
+  }, [setSessionContext, history]);
+
   return (<h1>oidc callback</h1>)
 }
