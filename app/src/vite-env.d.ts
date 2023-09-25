8f44210b0911778e344785345049de53f1b1795c
TeBeCo <tebeco@gmail.com>
2023-09-25 21:18:11 +0400

    add types/typings for .env configuration

--- app/src/vite-env.d.ts
+++ app/src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_OIDC_AUTHORITY: string;
+  readonly VITE_OIDC_REDIRECT_URI: string;
+  readonly VITE_MOBILE_SCHEME: string;
+  readonly VITE_MOBILE_HOSTNAME: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
