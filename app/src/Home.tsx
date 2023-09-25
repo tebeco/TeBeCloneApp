046507324c13c17f54740a5690151befb6a13757
TeBeCo <tebeco@gmail.com>
2023-09-26 00:09:38 +0400

    Home page new show the user name and offer a SignOut button

--- app/src/Home.tsx
+++ app/src/Home.tsx
@@ -1,3 +1,16 @@
-export const Home = () => (
-  <>Hello from home</>
-);
+import { useHistory } from "react-router";
+import { defaultSession, useSessionContext } from './contexts/SessionContext';
+
+export const Home = () => {
+  const [sessionContext, setSessionContext] = useSessionContext();
+
+  const history = useHistory();
+
+  return (<>
+    <p>Hello {sessionContext.userName}</p>
+    <button onClick={() => {
+      setSessionContext(defaultSession)
+      history.push('/')
+    }}>sign out</button>
+  </>);
+}
