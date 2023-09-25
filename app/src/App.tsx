30b5046e49f99dd40d66110937c20bd0a0cc6516
TeBeCo <tebeco@gmail.com>
2023-09-25 23:35:12 +0400

    adding BrowserRouter to App.tsx

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { IonApp, setupIonicReact } from '@ionic/react';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+setupIonicReact();
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <IonApp>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" render={() => (<>this is the root page</>)} />
+        </Switch>
+      </BrowserRouter>
+    </IonApp>
+  );
+};
 
 export default App
